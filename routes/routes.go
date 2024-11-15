a9ddda556cd8979bbbaac768c54f9ba4a63d7067
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-11-15 17:11:33 +0700

    Add routes
    Added these routes:
    > /auth/google
    > /auth/google/callback

---
+++ routes/routes.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"net/http"
+	"tangapp-be/controllers"
+	"tangapp-be/middleware"
+
+	"github.com/gin-gonic/gin"
+	"github.com/markbates/goth/gothic"
+)
+
+func SetupRoutes(r *gin.Engine) {
+	// Google Oauth routes
+	r.GET("/auth/:provider", func(c *gin.Context) {
+		gothic.GetProviderName = func(req *http.Request) (string, error) {
+			return "google", nil // specify the provider (e.g., Google)
+		}
+		gothic.BeginAuthHandler(c.Writer, c.Request)
+	})
+	r.GET("/auth/:provider/callback", controllers.GoogleAuthCallback)
+
+	// Protected routes
+	protected := r.Group("/protected")
+	protected.Use(middleware.JWTAuthMiddleware())
+	protected.GET("/profile", func(c *gin.Context) {
+		userID, _ := c.Get("userID")
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome to your profile!", "user_id": userID})
+	})
+
+}
