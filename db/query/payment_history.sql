7a958cc2e7ae6411a8a9c0e3b9fbb0c15a0d6d39
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-11-26 17:54:18 +0700

    Added new queries for payment_history :
    > ListPaymentHistoryByEvent
    > ListPaymentHistoryByUser

--- db/query/payment_history.sql
+++ db/query/payment_history.sql
@@ -1,11 +1,12 @@
 -- name: AddPaymentHistory :one
 INSERT INTO payment_history
-( event_member_details_id, to_user_id, nominal, description, created_at)
+( event_member_details_id, from_user_id, to_user_id, nominal, description)
 VALUES( $1, $2, $3, $4, $5)
 RETURNING *;
 
--- name: ListPaymentHistory :many
+-- name: ListPaymentHistoryByEvent :many
 SELECT 
+  ph.from_user_id,
   ph.to_user_id,
   ph.nominal, 
   ph.description, 
@@ -13,15 +14,28 @@ SELECT
 FROM payment_history ph 
 JOIN event_member_details emd on  ph.event_member_details_id = emd.id 
 JOIN events e on emd.event_id = e.id  
-WHERE e.id=$1;
+WHERE e.id=sqlc.arg(event_id);
+
+-- name: ListPaymentHistoryByUser :many
+SELECT 
+  e.id,
+  ph.from_user_id,
+  ph.to_user_id,
+  ph.nominal, 
+  ph.description, 
+  ph.created_at 
+FROM payment_history ph 
+JOIN event_member_details emd on  ph.event_member_details_id = emd.id 
+JOIN events e on emd.event_id = e.id  
+WHERE ph.from_user_id=$1 OR ph.to_user_id=$2;
+
 
 -- name: UpdatePaymentHistory :one
 UPDATE payment_history
 SET 
-  event_member_details_id= $2, 
+  event_member_details_id= $2,  
   to_user_id=$3, 
   nominal=$4,
-  description= $5, 
-  created_at= $6
+  description= $5
 WHERE id=$1
 RETURNING *;
