6c035442cb30e01a7d9a6db6b4e946b92e2bfee0
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-12-06 17:53:24 +0700

    Added users router :
    > /users

---
+++ modules/users/router/router.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"tangapp-be/modules/users/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUserRoutes(r *gin.Engine, userController *controller.UserController) {
+	users := r.Group("/v1/users")
+	{
+		users.POST("/", userController.CreateUser)
+	}
+}
