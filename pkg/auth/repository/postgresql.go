f323388d8f048f608047f992aba0301d50455537
RifqiNotWibu <rifqifadhillahh@gmail.com>
2024-12-09 16:29:06 +0700

    Refactored auth's repository due to change of repository to queries

--- pkg/auth/repository/postgresql.go
+++ pkg/auth/repository/postgresql.go
@@ -1,7 +1,7 @@
 package postgresql
 
 import (
-	"tangapp-be/repository"
+	"tangapp-be/queries"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -9,13 +9,13 @@ import (
 // Auth defines a postgres repository with the required dependencies.
 type Auth struct {
 	conn *pgxpool.Pool
-	q    *repository.Queries
+	q    *queries.Queries
 }
 
 // NewAuth returns an instance of Todo repository.
 func NewAuth(conn *pgxpool.Pool) *Auth {
 	return &Auth{
 		conn: conn,
-		q:    repository.New(conn),
+		q:    queries.New(conn),
 	}
 }
